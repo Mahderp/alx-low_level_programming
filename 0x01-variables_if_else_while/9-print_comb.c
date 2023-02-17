733df0d41ee2a106f33c7260f9f3acd0aa08ebfd
mahderp <mahderp1@gmail.com>
2023-02-17 13:51:47 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,3 +1,10 @@
+/*
+ * File: 9-print_comb.c
+ * It's mahder
+ */
+
+#include <stdio.h>
+
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -9,12 +16,13 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-	putchar((num % 10) + '0');
-	if (num == 9)
-	continue;
-	putchar(',');
-	putchar(' ');
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
+
 	return (0);
 }
